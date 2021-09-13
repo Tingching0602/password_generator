8f4969dea2840bebd98c3b3ba8c18ee964f2d12c
content Tingching0602 <contentanita2131love@gmail.com>
2021-09-13 18:25:49 +0800

    start to track

---
+++ app.js
@@ -0,0 +1,30 @@
+// app.js
+// include packages and define server related variables
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const generatePassword = require('./generate_password')
+const app = express()
+const port = 3000
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// use body-parser
+app.use(express.urlencoded({ extended: true }))
+
+// setting routes
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const password = generatePassword(req.body)
+  res.render('index', { password: password })
+})
+
+// starts the express server and listening for connections.
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}.`)
+})
---
+++ generate_password.js
@@ -0,0 +1,54 @@
+// generate_password.js
+function sample(collection) {
+  const randomIndex = Math.floor(Math.random() * collection.length)
+  return collection[randomIndex]
+}
+// define generatePassword function
+function generatePassword(options) {
+
+  // define things user might want
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+  const numbers = '1234567890'
+  const symbols = '`~!@$%^&*()-_+={}[]|;:"<>,.?/'
+
+
+  // create a collection to store things user picked up
+  let collection = []
+
+  if (options.lowercase === 'on') {
+    collection = collection.concat(lowerCaseLetters.split(''))
+  }
+
+  if (options.uppercase === 'on') {
+    collection = collection.concat(upperCaseLetters.split(''))
+  }
+
+  if (options.numbers === 'on') {
+    collection = collection.concat(numbers.split(''))
+  }
+
+  if (options.symbols === 'on') {
+    collection = collection.concat(symbols.split(''))
+  }
+
+  // remove things user do not need
+  if (options.excludeCharacters) {
+    // console.log(`exclude characters: ${options.excludeCharacters}`)
+    collection = collection.filter(
+      character => !options.excludeCharacters.includes(character)
+    )
+  }
+
+  // start generating password
+  let password = ''
+  for (let i = 1; i <= options.length; i++) {
+    password += sample(collection)
+  }
+
+  // return the generated password
+  return password
+  // console.log('collection', collection)
+}
+// invoke generatePassword function 
+module.exports = generatePassword
